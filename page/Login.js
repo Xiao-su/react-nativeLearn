196f4e360e2a7a5db3c89f6844746c038d4a569f
Xiao-su <1538455147@qq.com>
2021-06-19 19:09:49 +0800

    feat: 添加手机号码验证功能

--- page/Login.js
+++ page/Login.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Text, View, Image, StatusBar, StyleSheet } from 'react-native'
-import {pxToDp} from '../untils/stylesKits'
+import {pxToDp} from '../untils/stylesKits';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import phoneValidUntil from '../untils/validator'
 
 const styles = StyleSheet.create({
   topImg:{
@@ -19,7 +22,30 @@ const styles = StyleSheet.create({
 
 
 class Login extends Component {
+
+
+  state = {
+    phoneNumber: '',
+    phoneValid: true,
+  }
+
+  phoneChangeHandle = (Text) => {
+    this.setState({
+      phoneNumber: Text
+    })
+  }
+
+  phoneSubmitHandle = () => {
+    const { phoneNumber } = this.state;
+    const _phoneValid = phoneValidUntil.validatePhone(phoneNumber);
+    
+    this.setState({
+      phoneValid: _phoneValid
+    });
+  }
+
   render() {
+    const { phoneNumber, phoneValid } = this.state;
     return (
       <View>
         <StatusBar
@@ -30,6 +56,16 @@ class Login extends Component {
           <View>
             <Text style={styles.textA}>手机号登录注册</Text>
           </View>
+          <Input
+            value={phoneNumber}
+            placeholder='请输入手机号码'
+            maxLength={11}
+            keyboardType="phone-pad"
+            onChangeText={this.phoneChangeHandle}
+            errorMessage={phoneValid? null:"手机号码不正确"}
+            onSubmitEditing={this.phoneSubmitHandle}
+            leftIcon={{ type: 'font-awesome', name: 'phone', color: '#ccc', size: pxToDp(20) }}
+          />
         </View>
       </View>
     )
